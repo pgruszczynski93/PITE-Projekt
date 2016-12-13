9011e85e3b9ccb2c7c72389ba1a22533883b55cf
pgruszczynski93 <pgruszczynski93@gmail.com>
2016-12-13 19:49:07 +0100

    Add files via upload

--- gui/ImagePreProcessor.py
+++ gui/ImagePreProcessor.py
@@ -1,9 +1,9 @@
+from PIL import ImageFile
 from PIL import Image
 import PIL.ImageOps
 import math
 import numpy
 import datetime
-from PIL import ImageFile
 from Modals import *
 
 ImageFile.LOAD_TRUNCATED_IMAGES = True
@@ -14,9 +14,13 @@ class ImagePreProcessor(object):
 	def __init__(self):
 		self.image = None
 		self.pixels = None
-		self.modal_window = Modal()
+		self.modal_window = None
 		self.contrast_mod_value = 0
-
+		self.posterize_mod_value = 0
+		self.solarization_mod_value = 0
+		self.crop_opsborder_mod_value = 0
+		self.frame_color_width = 0
+		self.resize_mod_dim = ()
 
 	def loadImage(self, imgFile, isGray=1):
 		self.image = Image.open(imgFile)
@@ -33,12 +37,17 @@ class ImagePreProcessor(object):
 	def auto_contrast(self):
 		# pobawic sie ta wartoscia jako drugi argument, jeszcze 3ci opcjonalny
 		# false bo inaczej nie dziala XDD
+		self.modal_window = Modal("Kontrast: ")
+		self.modal_window.init_modal(0,50,0,50,1,"Zmiana kontrastu")
 		if self.modal_window.exec_() == False:
 			self.contrast_mod_value = self.modal_window.button_confirm_exit()
 			# print("prepre " + str(self.contrast_mod_value))
 			auto_contrast_img = PIL.ImageOps.autocontrast(self.image,int(self.contrast_mod_value))
 			self.image = auto_contrast_img
 			self.loadImageFromPIX(self.image)
+
+		# chamskie ale sprawdzenie
+		# del(self.modal_window) 
 
 	def auto_grayscale(self):
 		grayscale_img = PIL.ImageOps.grayscale(self.image)
@@ -47,7 +56,10 @@ class ImagePreProcessor(object):
 
 	def auto_colorize(self):
 		# ma pracowac na zdjeciu w odcieniach szarosci
-		colorize_img = PIL.ImageOps.colorize(self.image, black="#000099", white="#99CCFF")
+		self.modal_window = Modal()
+		black = self.modal_window.init_color_picker()
+		white = self.modal_window.init_color_picker()
+		colorize_img = PIL.ImageOps.colorize(self.image, black, white)
 		self.image = colorize_img
 		self.loadImageFromPIX(self.image)
 
@@ -63,14 +75,59 @@ class ImagePreProcessor(object):
 
 	def auto_posterize(self):
 		# ustawić prog posteryzacji jakims suwakeim - to samo solaryzacja
-		posterize_img = PIL.ImageOps.posterize(self.image, 2)
-		self.image = posterize_img
-		self.loadImageFromPIX(self.image)
+		self.modal_window = Modal("Bity kanałów: ")
+		self.modal_window.init_modal(1,8,1,8,1,"Posteryzacja")
+		if self.modal_window.exec_() == False:
+			self.posterize_mod_value = self.modal_window.button_confirm_exit()
+			# print("prepre " + str(self.posterize_mod_value))
+			posterize_img = PIL.ImageOps.posterize(self.image, self.posterize_mod_value)
+			self.image = posterize_img
+			self.loadImageFromPIX(self.image)
 
 	def auto_solarize(self):
-		solarize_img = PIL.ImageOps.solarize(self.image, 32)
-		self.image = solarize_img
+		self.modal_window = Modal("Próg solaryzacji ")
+		self.modal_window.init_modal(0,128,0,128,1,"Solaryzacja")
+		if self.modal_window.exec_() == False:
+			self.solarization_mod_value = self.modal_window.button_confirm_exit()
+			solarize_img = PIL.ImageOps.solarize(self.image, self.solarization_mod_value)
+			self.image = solarize_img
+			self.loadImageFromPIX(self.image)
+
+	def auto_delete_border(self):
+		self.modal_window = Modal("Piksele do usunięcia ")
+		self.modal_window.init_modal(0,200,0,200,1,"Usuwanie ramki zdjęcia")
+		if self.modal_window.exec_() == False:
+			self.crop_opsborder_mod_value = self.modal_window.button_confirm_exit()
+			crop_img = PIL.ImageOps.crop(self.image, self.crop_opsborder_mod_value)
+			self.image = crop_img
+			self.loadImageFromPIX(self.image)
+
+	def auto_equalize_histogram(self):
+		# ewentualnie dodać maske jako drugi argument
+		equal_hist_img = PIL.ImageOps.equalize(self.image)
+		self.image = equal_hist_img
 		self.loadImageFromPIX(self.image)
+
+	def auto_add_color_border(self):
+		self.modal_window = Modal("Grubość ramki ")
+		self.modal_window.init_modal(0,200,0,200,1,"Kolorowa ramka zdjęcia")
+		if self.modal_window.exec_() == False:
+			self.frame_color_width = self.modal_window.button_confirm_exit()
+			frame_color = self.modal_window.init_color_picker()
+			framed_img = PIL.ImageOps.expand(self.image, self.frame_color_width, frame_color)
+			self.image = framed_img
+			self.loadImageFromPIX(self.image)
+
+	def auto_fitscale(self):
+		self.modal_window = Modal()
+		self.modal_window.init_resize_modal("Skalowanie i dopasowanie")
+		if self.modal_window.exec_() == False:
+			self.resize_mod_dim = self.modal_window.button_resize_confirm_exit()
+			# sprawdzenie dla fit zmienic ew Image.resize
+			resized_img = PIL.ImageOps.fit(self.image, (int(self.resize_mod_dim[0]), int(self.resize_mod_dim[1])))
+			self.image = resized_img
+			# print(self.resize_mod_dim[0],self.resize_mod_dim[1])
+			self.loadImageFromPIX(self.image)
 
 	def save_photo_normal(self):
 		# domyslny zapis zdjecia pod skrótem ctrl+s
