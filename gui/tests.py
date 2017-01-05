48e41ae4318c77324bed661bec4ae694eb0458fe
Sekobb <majewski.jakub@tlen.pl>
2017-01-05 22:09:59 +0100

    Update tests.py

--- gui/tests.py
+++ gui/tests.py
@@ -1,1 +1,10 @@
+import unittest
+from ImagePreProcessor import *
 
+class tests(unittest.TestCase):
+  
+  def setUp(self):
+    self.imgProcessor = ImagePreProcessor()
+    
+  def init_test(self):
+    self.assertIsInstance(self.imgProcessor, ImagePreProcessor)
