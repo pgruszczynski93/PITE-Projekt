d5dfc794a89bdec6e14c049ebc6f95993764e812
Sekobb <majewski.jakub@tlen.pl>
2017-01-07 14:10:13 +0100

    Update tests.py

--- gui/tests.py
+++ gui/tests.py
@@ -1,5 +1,6 @@
 import unittest
 from ImagePreProcessor import *
+from MainWindow import *
 
 class tests(unittest.TestCase):
   
@@ -46,10 +47,31 @@ class tests(unittest.TestCase):
 		self.assertEqual(self.imgProcessor.noise_mod_value, 0)
 		self.assertEqual(self.imgProcessor.marker_mod_values, None)
 
-	def test_image_load(self):
+	def test_MainWindow_creation(self):
+		print("Sprawdzenie, czy poprawnie tworzy się okno główne programu")
+		app = QtGui.QApplication(sys.argv)
+		MainWindow = QtGui.QMainWindow()
+		ui = Ui_MainWindow()
+		ui.setupUi(MainWindow)
+
+	def test_image_operations(self):
 		print("Sprawdzenie funkcji wczytującej obraz wejściowy")
 		self.imgProcessor.loadImage("./image.jpg")
-
+		print("Sprawdzenie rozmiarów obrazu wejściowego")
+		self.assertEqual(self.imgProcessor.get_width(), 1000)
+		self.assertEqual(self.imgProcessor.get_height(), 500)
+		print("Sprawdzenie operacji tworzenia negatywu")
+		self.imgProcessor.negative()
+		#print("Sprawdzenie operacji zmiany kontrastu")
+		#self.imgProcessor.auto_contrast()
+		#print("Sprawdzenie operacji konwersji do skali szarości")
+		#self.imgProcessor.auto_grayscale()
+		#print("Sprawdzenie operacji koloryzacji")
+		#self.imgProcessor.auto_colorize()
+		#print("Sprawdzenie operacji lustra")
+		#self.imgProcessor.auto_mirror()
+		#print("Sprawdzenie operacji przerzucania")
+		#self.imgProcessor.auto_flip()
 
 
 if __name__ == '__main__':
